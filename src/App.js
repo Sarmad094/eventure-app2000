19d7b951fba71a0a9850730a05141c18698a378c
Sahil765804 <singh.sahil231004@gmail.com>
2025-01-23 17:33:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
       <div>
         <nav>
           <ul>
-            <li><Link to="/">Home1</Link></li>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/user/1">User 1</Link></li>
             <li><Link to="/user/2">User 2</Link></li>
           </ul>
